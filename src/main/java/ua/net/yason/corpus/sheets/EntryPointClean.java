bd1c6c947caebc80a61a658bc52eec346abde4bd
Sergey <yason2000@gmail.com>
2018-03-30 23:08:40 +0300

    fix import

--- src/main/java/ua/net/yason/corpus/sheets/EntryPointClean.java
+++ src/main/java/ua/net/yason/corpus/sheets/EntryPointClean.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
